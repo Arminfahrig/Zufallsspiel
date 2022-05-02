d90b605e2291de32fde501d3d77248d1727b432b
fahrigar <jkbk>
2022-05-02 10:12:13 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let zufallzahl1 = Math.round(Math.random()*100)
+let zufallszahl2 = Math.round(Math.random()*100)
+let winstreak
+let startwert = 50
+
+function höher(){
+    if(zufallzahl1 > startwert) {
+        document.getElementById("tf")
+    }
+}
